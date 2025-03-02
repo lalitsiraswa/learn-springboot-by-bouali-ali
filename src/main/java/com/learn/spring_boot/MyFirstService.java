9fe585b8ac14dff9bc6f17195b58b90c9fdd472b
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-03-02 23:19:11 +0530

    Field injection

--- src/main/java/com/learn/spring_boot/MyFirstService.java
+++ src/main/java/com/learn/spring_boot/MyFirstService.java
@@ -1,15 +1,12 @@
 package com.learn.spring_boot;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
-    private final MyFirstComponent firstComponent;
-
-    public MyFirstService(MyFirstComponent firstComponent) {
-        this.firstComponent = firstComponent;
-    }
+    @Autowired
+    private MyFirstComponent firstComponent;
 
     public String tellAStory() {
         return "The dependency is saying : " + firstComponent.sayHello();
