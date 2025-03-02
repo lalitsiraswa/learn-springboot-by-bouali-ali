d091d684290b9b75aa89df73ea554e80e2ec9f10
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-03-02 22:17:57 +0530

    Constructor injection no longer required @Autowired

--- src/main/java/com/learn/spring_boot/MyFirstService.java
+++ src/main/java/com/learn/spring_boot/MyFirstService.java
@@ -1,13 +1,11 @@
 package com.learn.spring_boot;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
     private final MyFirstComponent firstComponent;
 
-    @Autowired
     public MyFirstService(MyFirstComponent firstComponent) {
         this.firstComponent = firstComponent;
     }
