9952f9f9976ccaaefd582040068c15cf3e2b84a2
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-03-02 14:54:38 +0530

    Using @Component, @Service or @Repository for creating Bean

--- src/main/java/com/learn/spring_boot/MyFirstComponent.java
+++ src/main/java/com/learn/spring_boot/MyFirstComponent.java
@@ -1,8 +1,12 @@
 package com.learn.spring_boot;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 @Component
+//@Service
+//@Repository
 public class MyFirstComponent {
     public String sayHello() {
         return "Hello from the MyFirstComponent!";
