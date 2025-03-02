01c68540de35581350770790f6cfb1dd1e2adb03
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-03-02 13:32:26 +0530

    Creating my first @Bean

--- src/main/java/com/learn/spring_boot/Application.java
+++ src/main/java/com/learn/spring_boot/Application.java
@@ -2,12 +2,20 @@ package com.learn.spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+        MyFirstComponent firstComponent = applicationContext.getBean(MyFirstComponent.class);
+        System.out.println(firstComponent.sayHello());
     }
 
+    @Bean
+    public MyFirstComponent myFirstComponent() {
+        return new MyFirstComponent();
+    }
 }
---
+++ src/main/java/com/learn/spring_boot/MyFirstComponent.java
@@ -0,0 +1,7 @@
+package com.learn.spring_boot;
+
+public class MyFirstComponent {
+    public String sayHello() {
+        return "Hello from the MyFirstComponent!";
+    }
+}
