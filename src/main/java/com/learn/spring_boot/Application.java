acaa9dd2a82c2e4e611ad1c333ff2e1a821c5033
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-03-03 23:04:15 +0530

    Setting Spring Profile Programmatically

--- src/main/java/com/learn/spring_boot/Application.java
+++ src/main/java/com/learn/spring_boot/Application.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+        var application = new SpringApplication(Application.class);
+        application.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "dev"));
+//        ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext applicationContext = application.run(args);
 //        MyFirstComponent firstComponent = applicationContext.getBean(MyFirstComponent.class);
         MyFirstComponent firstComponent = applicationContext.getBean("myFirstComponent", MyFirstComponent.class);
         System.out.println(firstComponent.sayHello());
