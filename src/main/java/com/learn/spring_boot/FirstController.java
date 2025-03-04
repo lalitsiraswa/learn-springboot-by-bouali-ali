a08a291903fc4cfb1e81b4348558bc0b883c8680
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro-2.local>
2025-03-04 16:21:29 +0530

    First End-Point with @ResponseStatus(HttpStatus.ACCEPTED)

--- src/main/java/com/learn/spring_boot/FirstController.java
+++ src/main/java/com/learn/spring_boot/FirstController.java
@@ -1,7 +1,20 @@
 package com.learn.spring_boot;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World! From My First Controller.";
+    }
+
+    @GetMapping("/hello-2")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String sayHello2() {
+        return "Hello World2! First My First Controller.";
+    }
 }
