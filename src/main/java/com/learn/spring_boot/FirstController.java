a6f04defe48181453fe9f4284457ccce89460bce
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro-2.local>
2025-03-04 18:25:11 +0530

    Push latest changes

--- src/main/java/com/learn/spring_boot/FirstController.java
+++ src/main/java/com/learn/spring_boot/FirstController.java
@@ -1,8 +1,7 @@
 package com.learn.spring_boot;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +11,8 @@ public class FirstController {
         return "Hello World! From My First Controller.";
     }
 
-    @GetMapping("/hello-2")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public String sayHello2() {
-        return "Hello World2! First My First Controller.";
+    @PostMapping("/post")
+    public String post(String message) {
+        return "Request Accepted! and message is : " + message;
     }
 }
