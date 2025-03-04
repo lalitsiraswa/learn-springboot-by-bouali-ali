6eb38b0d419f471ad3a20d6710d7261ffdfe118b
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-03-04 19:01:28 +0530

    @PostMapping()

--- src/main/java/com/learn/spring_boot/FirstController.java
+++ src/main/java/com/learn/spring_boot/FirstController.java
@@ -2,6 +2,7 @@ package com.learn.spring_boot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,7 @@ public class FirstController {
     }
 
     @PostMapping("/post")
-    public String post(String message) {
+    public String post(@RequestBody String message) {
         return "Request Accepted! and message is : " + message;
     }
 }
