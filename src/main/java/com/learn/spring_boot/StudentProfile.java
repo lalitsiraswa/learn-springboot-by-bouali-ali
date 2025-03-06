e78ba7aabef2dc5e253577666757be18601e60d6
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-03-06 23:00:52 +0530

    @OneToOne Mapping Between Student & StudentProfile

--- src/main/java/com/learn/spring_boot/StudentProfile.java
+++ src/main/java/com/learn/spring_boot/StudentProfile.java
@@ -1,8 +1,6 @@
 package com.learn.spring_boot;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class StudentProfile {
@@ -11,6 +9,12 @@ public class StudentProfile {
     @GeneratedValue
     private Integer id;
     private String bio;
+
+    @OneToOne
+    @JoinColumn(
+            name = "student_id"
+    )
+    private Student student;
 
     public StudentProfile() {
     }
