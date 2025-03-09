fa71aba5c9af7a38df41cd115da25d537b3511f8
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-03-09 12:51:47 +0530

    CleanUp

--- src/test/java/com/learn/spring_boot/student/StudentMapperTest.java
+++ src/test/java/com/learn/spring_boot/student/StudentMapperTest.java
@@ -1,35 +1,5 @@
 package com.learn.spring_boot.student;
 
-import org.junit.jupiter.api.*;
-
 class StudentMapperTest {
-    @BeforeAll
-    static void beforeAll() {
-        System.out.println("Inside @BeforeAll Method!");
-    }
 
-    @AfterAll
-    static void afterAll() {
-        System.out.println("Inside @AfterAll Method!");
-    }
-
-    @BeforeEach
-    void setUp() {
-        System.out.println("Inside @BeforeEach Method!");
-    }
-
-    @AfterEach
-    void tearDown() {
-        System.out.println("Inside @AfterEach Method!");
-    }
-
-    @Test
-    public void testToStudent() {
-        System.out.println("Testing toStudent() Method!");
-    }
-
-    @Test
-    public void testToStudentResponseDto() {
-        System.out.println("Testing testToStudentResponseDto() Method!");
-    }
 }
