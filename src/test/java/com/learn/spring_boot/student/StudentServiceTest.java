488716699a3e674e3b27424aa144a2887487a78a
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-03-10 07:51:04 +0530

    'should_successfully_save_a_student' @Test Using Mockito for StudentService

--- src/test/java/com/learn/spring_boot/student/StudentServiceTest.java
+++ src/test/java/com/learn/spring_boot/student/StudentServiceTest.java
@@ -1,9 +1,14 @@
 package com.learn.spring_boot.student;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class StudentServiceTest {
     //    Which service we want to test.
@@ -19,5 +24,25 @@ class StudentServiceTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void should_successfully_save_a_student() {
+//        Given
+        StudentDto studentDto = new StudentDto("Lalit", "Siraswa", "lalitsiraswa@gmail.com", 152);
+        Student student = new Student("Lalit", "Siraswa", "lalitsiraswa@gmail.com", 25);
+        Student savedStudent = new Student("Lalit", "Siraswa", "lalitsiraswa@gmail.com", 25);
+        savedStudent.setId(201);
+//        Mock the calls
+        when(studentMapper.toStudent(studentDto)).thenReturn(student);
+        when(studentRepository.save(student)).thenReturn(savedStudent);
+        when(studentMapper.toStudentResponseDto(savedStudent)).thenReturn(new StudentResponseDto("Lalit", "Siraswa", "lalitsiraswa@gmail.com"));
+//        When
+        StudentResponseDto studentResponseDto = studentService.saveStudent(studentDto);
+//        Then
+        assertNotNull(studentResponseDto);
+        assertEquals(studentDto.firstname(), studentResponseDto.firstname());
+        assertEquals(studentDto.lastname(), studentResponseDto.lastname());
+        assertEquals(studentDto.email(), studentResponseDto.email());
     }
 }
